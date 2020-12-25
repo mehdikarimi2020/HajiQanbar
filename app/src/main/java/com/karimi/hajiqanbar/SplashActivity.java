afefeff82611285624920a5adb7ae4fe0b2de81f
mehdikarimi2020 <76550291+mehdikarimi2020@users.noreply.github.com>
2020-12-25 13:38:10 +0330

    بعد از اضافه کردن اسپلش

---
+++ app/src/main/java/com/karimi/hajiqanbar/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.karimi.hajiqanbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        setContentView(R.layout.activity_splash);
+    }
+}
